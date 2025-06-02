832e324bbf4492ba285c6b630b58c764d281c931
manalhehe <luffylonlyd@gmail.com>
2025-06-02 21:27:50 +0200

    Description de tes changements

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function saluer() {
-  alert("Hello depuis JavaScript !");
-}
+const btn = document.getElementById('btnChange');
+const img = document.getElementById('imageTest');
+
+btn.addEventListener('click', () => {
+    // Exemple simple : change la source vers une autre image dans pics (à ajouter)
+    if (img.src.includes('example.jpg')) {
+        img.src = 'pics/example2.jpg';
+    } else {
+        img.src = 'pics/example.jpg';
+    }
+});
